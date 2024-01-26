c05a9f70766951cbc1634fff78165b15cb9d7a6b
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-26 17:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-//your JS code here. If required.
+const secretMessage = {
+  data: "Uifsf jt b tfdsfu nfttbhf!",
+  decryptionKey: "1",
+};
+
+const missionInfo = {
+  missionName: "Operation X",
+  agentCodeName: "Double-O",
+  location: "Top Secret Location",
+};
+
+// Encryption function
+function encryptMessage(message, key) {
+  let encryptedMessage = "";
+  for (let i = 0; i < message.length; i++) {
+    const char = message[i];
+    if (char.match(/[a-zA-Z]/)) {
+      const code = char.charCodeAt(0);
+      const shifted = code + parseInt(key, 10);
+      const encryptedChar = String.fromCharCode(shifted);
+      encryptedMessage += encryptedChar;
+    } else {
+      encryptedMessage += char;
+    }
+  }
+  return encryptedMessage;
+}
+
+// Decrypt the message from the simulated heap memory
+function decryptMessage() {
+  const decryptedMessage = encryptMessage(secretMessage.data, -secretMessage.decryptionKey);
+  return decryptedMessage;
+}
+
+// Access mission information
+function getMissionInfo() {
+  return missionInfo;
+}
+
+// Reveal the secret message
+const decryptedSecretMessage = decryptMessage();
+const missionDetails = getMissionInfo();
